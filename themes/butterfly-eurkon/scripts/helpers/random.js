2d6b25408d2d9d26ec232e07b56edd06bbcf5176
Can1425 <myqf1425@126.com>
2023-07-31 15:54:31 +0800

    upd

---
+++ themes/butterfly-eurkon/scripts/helpers/random.js
@@ -0,0 +1,34 @@
+hexo.extend.filter.register('after_render:html', function (data) {
+  try {
+    const posts = []
+    hexo.locals.get('posts').forEach(function (post) {
+      if (post.random !== false) {
+        posts.push({
+          'title': post.title,
+          'path': '/' + post.path,
+          'categories': post.categories.data.map(function (item) { return { 'name': item.name, 'path': '/' + item.path } }),
+          'tags': post.tags.data.map(function (item) { return { 'name': item.name, 'path': '/' + item.path } }),
+          'cover': post.cover,
+          // 'description': post.description,
+          // 'wordCount': post.content.replace(/<(?:.|\s)*?>/g, '').length,
+          // 'content': strip_html(post.content),
+          'date': post.date,
+          'updated': post.updated
+        })
+      }
+    })
+    data += `<script>var posts=${JSON.stringify(posts)};function toRandomPost(){ window.pjax ? pjax.loadUrl(posts[Math.floor(Math.random()*posts.length)].path) : window.open(posts[Math.floor(Math.random()*posts.length)].path, "_self"); };</script>`
+  } catch (e) { } finally { return data }
+})
+
+hexo.extend.filter.register('after_render:html', function (data) {
+  try {
+    const flinks = []
+    hexo.locals.get('data').link.forEach(function (list) {
+      list.link_list.map(function (flink) {
+        flinks.push(flink)
+      })
+    })
+    data += `<script>var flinks=${JSON.stringify(flinks)};function toRandomFlink(){window.open(flinks[Math.floor(Math.random()*flinks.length)].link);};</script>`
+  } catch (e) { } finally { return data }
+})
